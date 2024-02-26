ee84405ff4a78af3e55fb32ae731946fd4d36d5d
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-02-26 12:09:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+ 
+function addColors(e) {
+  squares.forEach((square) => {
+    square.style.backgroundColor = square === e.target ? "#E6E6FA" : "#6F4E37";
+  });
+}
+ 
+function resetColors() {
+  squares.forEach((square) => {
+    square.style.backgroundColor = "#E6E6FA";
+  });
+}
+ 
+const squares = document.querySelectorAll(".square");
+ 
+squares.forEach((e) => {
+  e.addEventListener("mouseover", addColors);
+  e.addEventListener("mousedown", resetColors);
+  e.addEventListener("mouseout", resetColors);
+});
